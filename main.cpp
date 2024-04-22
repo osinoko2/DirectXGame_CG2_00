e9cd0fb565bd2762640f9159298993b8487a5270
osinoko <k023g0114@g.neec.ac.jp>
2024-04-22 15:52:11 +0900

    オブジェクトを解放する

--- main.cpp
+++ main.cpp
@@ -172,21 +172,20 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// エラー時に止まる
 		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, true);
 		// 警告時に止まる
-		infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
-		// 
+		//infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, true);
+		// 抑制するメッセージのID
 		D3D12_MESSAGE_ID denyIds[] = {
-			// 
-			D3D12_MESSAGE_ID_RESOURCE_BARRIER_MATCHING_COMMAND_LIST_TYPE
-        };
-		// 
+			D3D12_MESSAGE_ID_RESOURCE_BARRIER_MISMATCHING_COMMAND_LIST_TYPE
+		};
+		// 抑制するレベル
 		D3D12_MESSAGE_SEVERITY severities[] = { D3D12_MESSAGE_SEVERITY_INFO };
 		D3D12_INFO_QUEUE_FILTER filter{};
 		filter.DenyList.NumIDs = _countof(denyIds);
 		filter.DenyList.pIDList = denyIds;
 		filter.DenyList.NumSeverities = _countof(severities);
 		filter.DenyList.pSeverityList = severities;
-		// 
-		infoQueue->PushStorsgeFilter(&filter);
+		// 指定したメッセージの表示を抑制する
+		infoQueue->PushStorageFilter(&filter);
 		// 解放
 		infoQueue->Release();
 	}
@@ -335,32 +334,34 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			DispatchMessage(&msg);
 		}
 		else {
-			CloseHandle(fenceEvent);
-			fence->Release();
-			rtvDescriptorHeap->Release();
-			swapChainResources[0]->Release();
-			swapChainResources[1]->Release();
-			swapChain->Release();
-			commandList->Release();
-			commandAllocater->Release();
-			commandQueue->Release();
-			device->Release();
-			useAdapter->Release();
-			dxgiFactory->Release();
-#ifdef _DEBUG
-			debugController->Release();
-#endif 
-			CloseWindow(hwnd);
-			// リソースリークチェック
-			IDXGIDebug1* debug;
-			if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug))))
-			{
-				debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
-				debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
-				debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
-				debug->Release();
-			}
-			return 0;
+			
 		}
 	}
+
+	CloseHandle(fenceEvent);
+	fence->Release();
+	rtvDescriptorHeap->Release();
+	swapChainResources[0]->Release();
+	swapChainResources[1]->Release();
+	swapChain->Release();
+	commandList->Release();
+	commandAllocater->Release();
+	commandQueue->Release();
+	device->Release();
+	useAdapter->Release();
+	dxgiFactory->Release();
+#ifdef _DEBUG
+	debugController->Release();
+#endif 
+	CloseWindow(hwnd);
+	// リソースリークチェック
+	IDXGIDebug1* debug;
+	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&debug))))
+	{
+		debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+		debug->Release();
+	}
+	return 0;
 }
