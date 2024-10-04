d1ef534fb03b5261d1034bec2773cbb5666662f7
osinoko <k023g0114@g.neec.ac.jp>
2024-10-04 11:51:44 +0900

    2値抜き

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,6 +1,7 @@
 #include "object3d.hlsli"
 
-struct Material{
+struct Material
+{
     float4 color;
     int enableLighting;
     float4x4 uvTransform;
@@ -38,9 +39,22 @@ PixelShaderOutput main(VertexShaderOutput input)
         // half lambert
         float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
-        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        //output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
         output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
         output.color.a = gMaterial.color.a * textureColor.a;
+        
+        if (textureColor.a <= 0.5)
+        {
+            discard;
+        }
+        //if (textureColor.a == 0.0)
+        //{
+        //    discard;
+        //}
+        if (output.color.a == 0.0)
+        {
+            discard;
+        }
     }
     else
     { // Lightingしない場合。前回までと同じ演算
