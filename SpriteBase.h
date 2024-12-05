675d51317acc762209e4aea56ae3cbeb5b64a031
osinoko <k023g0114@g.neec.ac.jp>
2024-12-05 15:15:58 +0900

    スプライト共通部

--- SpriteBase.h
+++ SpriteBase.h
@@ -6,5 +6,21 @@ class SpriteBase
 public:
 	// 初期化
 	void Initialize(DirectXBase* dxBase);
+
+	// 共通描画設定
+	void CommonDrawingSettings();
+
+	DirectXBase* GetDxBase() const { return dxBase_; }
+private:
+	// ルートシグネチャの作成
+	void CreateRootSignature();
+	// グラフィックスパイプラインの生成
+	void GenerateGraphicsPipeline();
+
+	DirectXBase* dxBase_;
+	ID3DBlob* signatureBlob = nullptr;
+	ID3DBlob* errorBlob = nullptr;
+	ID3D12RootSignature* rootSignature = nullptr;
+	ID3D12PipelineState* graphicsPipelineState = nullptr;
 };
 
